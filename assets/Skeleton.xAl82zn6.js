83bbf42c2ff0e3a9b3fcca8d5630dcb32aff5713
markjin9981 <pipj601@gmail.com>
2026-01-28 18:37:12 +0900

    Updates

--- assets/Skeleton.xAl82zn6.js
+++ assets/Skeleton.xAl82zn6.js
@@ -1,4 +1,4 @@
-import{c,j as e}from"./index.DdSTBng8.js";/**
+import{c,j as e}from"./index.Cj3uOhlU.js";/**
  * @license lucide-react v0.562.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/wallet.D0wO0c9j.js
+++ assets/wallet.D0wO0c9j.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index.DdSTBng8.js";/**
+import{c as a}from"./index.Cj3uOhlU.js";/**
  * @license lucide-react v0.562.0 - ISC
  *
  * This source code is licensed under the ISC license.
