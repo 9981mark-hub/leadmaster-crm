2f251bb856828789b9ca084322610e37d2d36e38
markjin9981 <pipj601@gmail.com>
2026-01-28 11:26:06 +0900

    Updates

--- assets/chevron-right-BKSc5KYO.js
+++ assets/chevron-right-BKSc5KYO.js
@@ -1,4 +1,4 @@
-import{c as o}from"./index-Bhxa7H-8.js";/**
+import{c as o}from"./index-D_FM4sWd.js";/**
  * @license lucide-react v0.562.0 - ISC
  *
  * This source code is licensed under the ISC license.
